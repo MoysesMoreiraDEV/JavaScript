e401d172b4388b112efe5a17d75c4721a9acd412
Moyses Moreira <137463806+MoysesMoreiraDEV@users.noreply.github.com>
2023-07-10 20:03:53 -0300

    Initial commit

---
+++ Curso web moderno JS/inicio/Web_moderno019/comentario.js
@@ -0,0 +1,4 @@
+//console.log é usado para exibir algo na tela do computador.
+console.log("Bom dia!");
+console.log("Boa tarde!");
+console.log("Boa noite!");
---
+++ Curso web moderno JS/inicio/Web_moderno023-26/dados.js
@@ -0,0 +1,11 @@
+var characterName = "higuso"
+var vida = 100;
+var ataque = 72.4;
+var manaPorSegundo = 0.5;
+var velocidadeMovimento = 40.5
+
+console.log(characterName);
+console.log(vida);
+console.log(ataque);
+console.log(manaPorSegundo);
+console.log(velocidadeMovimento);
